d43115e3ae734f33e2ddf0d022bc00aa557bbce0
Neil Seejoor <nvseejoor@gmail.com>
2018-08-12 21:02:32 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,6 +4,6 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    Boat.limit(5).order('id aces')
+    Boat.limit(5).order('id acs')
   end
 end
